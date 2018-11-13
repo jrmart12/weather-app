cb4c8200827c3314bccc5c337fff071334028253
jose <rodolfojose@live.com>
2018-11-13 17:50:12 -0600

    casi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Form from './Form';
 import PropTypes from "prop-types";
 import axios from "axios";
 
@@ -11,18 +10,17 @@ class App extends React.Component{
     this.state = {
       weather:undefined,
       temperature:undefined,
-      locations:undefined,
       location:undefined
       }
     this.handleClick = this.handleClick.bind(this);
     }
   handleClick(event) {
     event.preventDefault();
-    const locationname = event.target.nameLocation.value;
-    if (locationname) {
+    const name = event.target.citylocation.value;
+    if (name) {
       axios
         .get(
-          `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${locationname}`
+          `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${name}`
         )
         .then(data => {
           const woeid = data.data[0].woeid;
@@ -32,50 +30,65 @@ class App extends React.Component{
             )
             .then(response => {
                 this.setState(() => ({
-                    city: response.data.title,
-                    condition: response.data.consolidated_weather[0].weather_state_name,
+                    location: response.data.title,
+                    weather: response.data.consolidated_weather[0].weather_state_name,
                     temperature: response.data.consolidated_weather[0].the_temp
                   }));
+            })
+            .catch(err => {
+              console.log(err.message);
             });
         })
+        .catch(err => {
+            this.setState(() => ({
+                location: 'no se encontro',
+                weather: '',
+                temperature: ''
+              }));
+          console.log(err.message);
+        });
     }
   }
 
-  fetchLocationId = async city => {
-    const response = await fetch(
-      `https://www.metaweather.com/api/location/search/?query=${city}`,
-    );
-    const locations = await response.json();
-    return locations[0].woeid;
-  };
-  
-  fetchWeather = async woeid => {
-    const response = await fetch(
-      `https://www.metaweather.com/api/location/${woeid}/`,
-    );
-    const { title, consolidated_weather } = await response.json();
-    const { weather_state_name, the_temp } = consolidated_weather[0];
-  
-    return {
-      location: title,
-      weather: weather_state_name,
-      temperature: the_temp,
-    };
-  };
   
 
 render(){
   return(
     <div className="center">
+    <div>
               <p>{this.state.location} </p>
               <p>{this.state.weather} </p>
               <p>{this.state.temperature} </p>
-              <Form city={this.handleClick}  />
-
+              </div>
+              <div>
+              <form onSubmit = {this.handleClick}>
+                    <input type="text" name="citylocation" placeholder="Location..."/>
+                    <button>Search Weather</button>
+              </form>
+      </div>
       </div> 
   )
 }
+
 }
+
+export const fetchLocationId = async city => {
+  const response = await fetch(`location/search/?query=${city}`);
+  const locations = await response.json();
+  return locations[0].woeid;
+};
+
+export const fetchWeather = async woeid => {
+  const response = await fetch(`location/${woeid}/`);
+  const { title, consolidated_weather } = await response.json();
+  const { weather_state_name, the_temp } = consolidated_weather[0];
+
+  return {
+    location: title,
+    weather: weather_state_name,
+    temperature: the_temp
+  };
+};
 App.propTypes = {
   match: PropTypes.object.isRequired
 };
